cddc232b5c39d290b39f89d25da74a4c368e0417
arturocas98 <arturocastro98@hotmail.com>
2020-09-27 23:33:27 -0500

    creando la vista de registro con diseño inicial

--- app/src/main/java/com/example/social_gaming/MainActivity.java
+++ app/src/main/java/com/example/social_gaming/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.social_gaming;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView go_register;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        go_register = (TextView) findViewById(R.id.register);
+        go_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this,register.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
