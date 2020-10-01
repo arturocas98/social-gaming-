29adc412a6e8a9f5682c341db08e62620c7e3485
arturocas98 <arturocastro98@hotmail.com>
2020-10-01 08:35:33 -0500

    validando la vista de registro correo y campos vacios

--- app/src/main/java/com/example/social_gaming/MainActivity.java
+++ app/src/main/java/com/example/social_gaming/MainActivity.java
@@ -4,16 +4,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
     TextView go_register;
+    TextInputEditText txt_email;
+    TextInputEditText txt_password;
+    Button btn_login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         go_register = (TextView) findViewById(R.id.register);
+        txt_email = findViewById(R.id.txt_email);
+        txt_password = findViewById(R.id.txt_password);
+        btn_login = findViewById(R.id.btn_login);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+
         go_register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -22,5 +40,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void login(){
+        String email = txt_email.getText().toString();
+        String password = txt_password.getText().toString();
+        Log.d("campos","email:"+email);
+        Log.d("campos","password:"+password);
     }
 }
--- app/src/main/java/com/example/social_gaming/register.java
+++ app/src/main/java/com/example/social_gaming/register.java
@@ -2,13 +2,74 @@ package com.example.social_gaming;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class register extends AppCompatActivity {
-
+    CircleImageView btn_back ;
+    TextInputEditText txt_user;
+    TextInputEditText txt_email_reg;
+    TextInputEditText txt_password_reg;
+    TextInputEditText txt_conf_passwor_reg;
+    Button btn_register;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        btn_back = findViewById(R.id.btn_back);
+        txt_user = findViewById(R.id.txt_user_reg);
+        txt_email_reg = findViewById(R.id.txt_email_reg);
+        txt_password_reg = findViewById(R.id.txt_password_reg);
+        btn_register = findViewById(R.id.btn_register);
+
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                register();
+            }
+        });
+
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
+
+    private void register(){
+        String user = txt_user.getText().toString();
+        String email = txt_email_reg.getText().toString();
+        String password = txt_password_reg.getText().toString();
+        String con_password = txt_conf_passwor_reg.getText().toString();
+        if (!user.isEmpty() && !email.isEmpty() && !password.isEmpty() && !con_password.isEmpty()){
+            if (isEmailValid(email)){
+                Toast.makeText(this,"Se ha registrado correctamente",Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(this,"El correo es invalido",Toast.LENGTH_SHORT).show();
+            }
+            //Toast.makeText(this,"Se ha registrado correctamente",Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this,"Llene todos los campos para registrarse",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public boolean isEmailValid(String email) {
+        String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
+        Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
+
 }
